89cb5ea6a9740d3add8cbf3f49d582ed2c98ab5d
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2025-02-18 10:32:04 +0100

    .

---
+++ src/split/odd_even_2D.py
@@ -0,0 +1,10 @@
+def odd_even_columns(image):
+    s1 = image[:, ::2]
+    s2 = image[:, 1::2]
+    return s1, s2
+
+def odd_even_rows(image)_
+    s1 = image[:, ::2]
+    s2 = image[:, 1::2]
+    return s1, s2
+
